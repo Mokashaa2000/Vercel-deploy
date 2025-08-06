f48c65862504be262ce6838807a2fd055cd4aba5
Mohammed Kashalo <55239590+Mokashaa2000@users.noreply.github.com>
2025-08-06 20:39:14 +0300

    Update landing-page-client.tsx

--- app/landing-page-client.tsx
+++ app/landing-page-client.tsx
@@ -109,13 +109,13 @@ export default function LandingPageClient() {
             </nav>
           </div>
         )}
-        <Script type="text/javascript">
+        <script type="text/javascript">
     (function(c,l,a,r,i,t,y){
         c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
         t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
         y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
     })(window, document, "clarity", "script", "rvhftyoatx");
-</Script>
+</script>
       </header>
 
       <main className="flex-1">
@@ -542,3 +542,4 @@ export default function LandingPageClient() {
   );
 
 }
+
